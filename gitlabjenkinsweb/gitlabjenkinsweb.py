fa85470d1b3c1ba4ce89d27712800d40ecb12ffb
Chris Hemphill <chemphill@shopzilla.com>
2014-03-20 17:51:53 -0700

    Added license

--- gitlabjenkinsweb/gitlabjenkinsweb.py
+++ gitlabjenkinsweb/gitlabjenkinsweb.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+# * Copyright 2012 Shopzilla.com
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# * http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# *
+# * http://tech.shopzilla.com
+# *
+
 import tornado.gen
 import tornado.httpclient
 from gitlabjenkins import BaseHandler
@@ -20,7 +37,6 @@ class WebHookHandler(BaseHandler):
             yield self.jenkins.delete_job(job_name)
             return
         tree = data['repository']['homepage'] + '/raw/' + data['after'] + '/'
-        branch = data['ref'].split('/')[-1]
         triggers = ['jenkins.xml']
         for filename in triggers:
             try:
