f9fdafa4f228805fd900db8e50ef85dd5a7ad80b
Chris Hemphill <chemphill@shopzilla.com>
2014-04-14 13:31:57 -0700

    updating copyright year to 2014

--- gitlabjenkinsweb/gitlabjenkinsweb.py
+++ gitlabjenkinsweb/gitlabjenkinsweb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# * Copyright 2012 Shopzilla.com
+# * Copyright 2014 Shopzilla.com
 # *
 # * Licensed under the Apache License, Version 2.0 (the "License");
 # * you may not use this file except in compliance with the License.
